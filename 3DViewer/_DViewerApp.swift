3471d3809f87aea5100e1f8a639959df5e8faf1d
lionshilov <47785492+lionshilov@users.noreply.github.com>
2024-09-04 11:46:20 +0700

    Initial Commit

---
+++ 3DViewer/_DViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DViewerApp.swift
+//  3DViewer
+//
+//  Created by Лев Шилов on 04.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct _DViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
